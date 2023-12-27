7888cd29a14c535ae28a9703b4aa03b4a6c83d2c
pawelb100 <pawel.borucki@outlook.com>
2023-12-27 16:45:42 +0100

    Auto Focus on input after added new task

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,19 +1,22 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { FormStyled, TaskContent, AddTaskButton } from './styled';
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         if (newTaskContent !== "")
             addNewTask(newTaskContent.trim());
         setNewTaskContent("");
+        inputRef.current.focus();
     };
 
     return (
         <FormStyled onSubmit={onFormSubmit}>
             <TaskContent
+                ref={inputRef}
                 placeholder="Co jest do zrobienia?"
                 value={newTaskContent}
                 onChange={(event) => setNewTaskContent(event.target.value)} />
