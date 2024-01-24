ef6dd41c3f987867622da4c63223d39a9f392757
pawelb100 <pawel.borucki@outlook.com>
2024-01-24 14:57:48 +0100

    added loading text on button while fetching example tasks

--- src/features/tasks/FetchTasksButton/index.js
+++ src/features/tasks/FetchTasksButton/index.js
@@ -1,15 +1,31 @@
 import Button from "../../../common/Button";
 import { useDispatch } from "react-redux";
 import { fetchExampleTasks } from "../tasksSlice";
+import { useState } from "react";
 
 const FetchTasksButton = () => {
+
+  const [title, setTitle] = useState("Pobierz przykładowe zadania");
+  const [disabled, setDisabled] = useState(false);
+
+  const onClickButton = () => {
+    setTitle("Ładowanie...");
+    setDisabled(true);
+
+    setTimeout(() => {
+      dispatch(fetchExampleTasks());
+      setTitle("Pobierz przykładowe zadania");
+      setDisabled(false);
+    }, 1000);
+  }
 
   const dispatch = useDispatch();
 
   return (
     <Button
-      title="Pobierz przykładowe zadania"
-      onClick={() => dispatch(fetchExampleTasks())}
+      title={title}
+      onClick={onClickButton}
+      isDisabled={disabled}
     />
   );
 };
